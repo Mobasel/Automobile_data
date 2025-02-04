f2b0b555bab49ea94182f3882c8cd7111d2c5f57
Mostafa ElBassel <Mostafa.Elbassel.88@gmail.com>
2025-02-05 00:14:56 +0200

    Delete Average_prices_by_DriveWheels.sql

--- Average_prices_by_DriveWheels.sql
+++
@@ -1,8 +0,0 @@
-SELECT [Drive-Wheels], ROUND(AVG(Price), 2) AS Average_prices_by_DriveWheels
-FROM Automobile_data
-GROUP BY [Drive-Wheels]
-ORDER BY Average_prices_by_DriveWheels DESC;
-
-/*
-Here we can see that cars with "rwd" Drive wheels type has the highest average prices.
-*/
